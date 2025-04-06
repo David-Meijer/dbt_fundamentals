ca0950b109e372478221d594370864eb15d069cb
David Meijer <david.meijer@aurai.com>
2025-04-06 21:28:56 +0200

    descriptions added

--- dbt_reference_project/models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ dbt_reference_project/models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -4,5 +4,5 @@ SELECT
      ID         AS order_id
     ,USER_ID    AS customer_id
     ,ORDER_DATE AS order_date
-    ,STATUS     AS order_date 
+    ,STATUS     AS status
 FROM {{source('jaffle_shop', 'jaffle_shop_orders')}}
